d4b054e1fa214f03f8cd2bb99de40a5f45696a4a
Nguyễn Minh Lý <82688630+lynguyenminh@users.noreply.github.com>
2023-03-09 21:19:43 +0700

    Update data.py

--- src/data.py
+++ src/data.py
@@ -4,11 +4,11 @@ import os
 model_path = 'models'
 
 # detection model
-face_detection_models_weight = ['face_detection_yolov5s.pt', 'None', 'haarcascade_frontalface_default.xml', 'orther_path']
-face_detection_model_name = ['yolo', 'mtcnn', 'Facecascade', 'orther_model']
+face_detection_models_weight = ['face_detection_yolov5s.pt', 'None', 'haarcascade_frontalface_default.xml']
+face_detection_model_name = ['yolo', 'mtcnn', 'Facecascade']
 detection_models_dict = {name: os.path.join(model_path, filee) for name, filee in zip(face_detection_model_name, face_detection_models_weight)}
 
 # Recogntion
-gender_recognition_models_weight = ['efficientnetB1.pt', 'efficientnetB3.pt', 'svm-recognition.pth', 'svm-facenet.pth', 'orther_path']
-gender_recognition_model_name = ['efficientnetb1', 'efficientnetb3', 'svm', 'facenet-svm', 'orther_model']
+gender_recognition_models_weight = ['efficientnetB1.pt', 'efficientnetB3.pt', 'svm-recognition.pth', 'svm-facenet.pth']
+gender_recognition_model_name = ['efficientnetb1', 'efficientnetb3', 'svm', 'facenet-svm']
 recognition_models_dict = {name: os.path.join(model_path, filee) for name, filee in zip(gender_recognition_model_name, gender_recognition_models_weight)}
